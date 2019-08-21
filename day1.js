1591f093b1798fd2b46803de78f9a2ab8c95e469
mohammed aljulayfi <you@example.com>
2019-08-21 05:34:10 +0300

    first day

---
+++ day1.js
@@ -0,0 +1,14 @@
+alert("hello from java script");
+document.write("this text is from document.write ");
+document.write("<br>");
+
+document.write("add in java script 50+60 : "+5+60);
+/*
+here it add as a text
+*/
+var x=5;
+var y=60;
+var z=x+y;
+document.write("<br>");
+
+document.write("add in java script 5+60= "+z);
