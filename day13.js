cd4e8148b3a936ab6bf939933bbf96d4e3b853b9
mohammed aljulayfi <you@example.com>
2019-09-02 10:29:01 +0300

    day 13 strings

---
+++ day13.js
@@ -0,0 +1,8 @@
+document.write('your name=\"mozaid\"');
+document.write("<br>")
+var st="hello,World"
+console.log(st.length)
+console.log(st.charAt(5))
+console.log(st.indexOf("l"))
+console.log(st.split(","))
+console.log(st.toUpperCase())
