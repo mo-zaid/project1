acab7b50e6ee636b792a10df97ce70bcd7be54ad
mohammed aljulayfi <you@example.com>
2019-09-04 23:30:23 +0300

    day 15 if

--- day14.js
+++ day14.js
@@ -1,17 +1,13 @@
-var x=20;
-var y="20";
+var x = 20;
+var y = "20";
 
-if (x==y)
-    {
-        console.log("x = y , same number ")
-    }
-else{
+if (x == y) {
+    console.log("x = y , same number ")
+} else {
     console.log("x != value with y")
 }
-if (x===y)
-    {
-        console.log("x=y same number and type")
-    }
-else{
+if (x === y) {
+    console.log("x=y same number and type")
+} else {
     console.log("x != value with y || x !== y with the type ")
 }
---
+++ day15.js
@@ -0,0 +1,13 @@
+var x = 10;
+var y = 20;
+var z = 30;
+if (x < y) {
+    if (x < z) {
+        console.log("x is sammlest");
+    }
+
+}
+
+if (x != z) {
+    console.log("x!=z")
+}
