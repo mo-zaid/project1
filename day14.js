bd973173ab34c84e0eb13820fc864fbf7f89c650
mohammed aljulayfi <you@example.com>
2019-09-04 23:20:54 +0300

    day 14 operator

---
+++ day14.js
@@ -0,0 +1,17 @@
+var x=20;
+var y="20";
+
+if (x==y)
+    {
+        console.log("x = y , same number ")
+    }
+else{
+    console.log("x != value with y")
+}
+if (x===y)
+    {
+        console.log("x=y same number and type")
+    }
+else{
+    console.log("x != value with y || x !== y with the type ")
+}
