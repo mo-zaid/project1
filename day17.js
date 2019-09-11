a1997a01ecf890e2cbe5a6f7b0fa9c61429c8043
mohammed aljulayfi <you@example.com>
2019-09-11 13:06:37 +0300

    day 18-19

--- day17.js
+++ day17.js
@@ -1,27 +1,30 @@
-var n=3;
-switch(n)
-    {
-        case 1:document.write("1 msg");
-            break;
-        case 2:document.write("2 msg");
-            break;
-        case 3:document.write("3 msg ");
-            break;
-        default:document.write("new msg");
-            break;
-    }
+var n = 3;
+switch (n) {
+    case 1:
+        document.write("1 msg");
+        break;
+    case 2:
+        document.write("2 msg");
+        break;
+    case 3:
+        document.write("3 msg ");
+        break;
+    default:
+        document.write("new msg");
+        break;
+}
 
-var x="wed"
+var x = "wed"
 
-switch(x)
-    {
-            case"sun":
-            case"mon":
-            case"tue":
-            case"wed":
-            case"thu":
-            document.write("work time");
-            break;
-        default:document.write("off");
-            break;
-    }
+switch (x) {
+    case "sun":
+    case "mon":
+    case "tue":
+    case "wed":
+    case "thu":
+        document.write("work time");
+        break;
+    default:
+        document.write("off");
+        break;
+}
---
+++ day18-19.js
@@ -0,0 +1,9 @@
+var browser = "firefox";
+
+if (browser == "edge") {
+    alert("you have got edge");
+} else if (browser == "chrome" || browser == "safari" || browser == "opera" || browser == "firefox") {
+    alert("we support these");
+} else {
+    alert("we hope its ok ");
+}
